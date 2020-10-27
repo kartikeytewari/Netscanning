42bc18b2ad0efd89f9e9560a21543f018c8227ac
Kartikey Tewari <kartikeya30@gmail.com>
2020-10-27 14:12:26 +0530

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+echo "Warning: Running this script will delete the old environment variables"
+echo "Ignore this warning if running the script for the first time"
+echo ""
+echo "Do you want to proceed? (y/n)"
+read flag
+if [[ $flag == y ]]
+then
+    echo "Deleting old envirnoment file"
+    rm .env
+    echo "Creating new environment file"
+    touch .env
+    g++ -o setup_dep.out setup_dep.cpp
+    ./setup_dep.out > .env
+    echo "Deleting dependencies"
+    rm ./setup_dep.out
+    echo "Setup succesfull"
+else
+    echo "Terminating script"
+fi
