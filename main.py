47a1fc894e08fb23811a7e95ccd477d07c3265a6
Kartikey Tewari <kartikeya30@gmail.com>
2020-10-27 12:56:13 +0530

    Added functionality in main script to dynamically monitor many devices device_ip, device_name and device_flag are used per flag in main script device_ip and device_name are set as environment variable in .env file

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import os
 from decouple import config
 
 IP_NETWORK = config('IP_NETWORK')
-shubhra_iphone = config('shubhra_iphone')
-chandra_big_iphone=config('chandra_big_iphone');
-shubhra_iphone_check=True
-chandra_big_iphone_check=True
+device_count=int(config('device_count'))
+device_ip=[]
+device_flag={}
+device_name=[]
+
+for i in range(0,device_count):
+	device_ip.append(config('device_ip[' + str(i) + ']'))
+	device_name.append(config('device_name[' + str(i) + ']'))
+	device_flag[i]=True
 
 proc = subprocess.Popen(["ping", IP_NETWORK], stdout=subprocess.PIPE)
 while True:
@@ -16,9 +21,7 @@ while True:
 		break
 
 	connected_ip = line.decode('utf-8').split()[3]
-	if connected_ip == shubhra_iphone and shubhra_iphone_check:
-	 	print("shubra iphone connected to the network")
-	 	shubhra_iphone_check=False
-	elif connected_ip == chandra_big_iphone and chandra_big_iphone_check:
-		print ("chandra iphone 6s+ connected to the netork")
-		chandra_big_iphone_check=False
+	for i in range(0,device_count):
+		if (connected_ip == str(device_ip[i]) and device_flag[i] == True):
+			print (str(device_name[i]) +  " is connected")
+			device_flag[i]=False
